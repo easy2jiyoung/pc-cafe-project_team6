314b580806e4e1660b6a6aecc7db2d7e553bc232
Kyeongjin-Park <cloud1178@naver.com>
2023-02-01 22:22:46 +0900

    User Repository 정리 초안

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const { createServer } = require('http');
 
+const userRouter = require("./routes/user.routes");
+
 const app = express();
 const port = 1004
 
@@ -14,7 +16,7 @@ const router = require('./routes');
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api', router);
+app.use('/api', [userRouter]);
 
 
 http.listen(port, () => {

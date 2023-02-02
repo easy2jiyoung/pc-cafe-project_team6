4970ae5b2fa41dceaec99152e8653b975e7ced8c
kangharu <harucometrue7@gmail.com>
2023-02-02 11:54:41 +0900

    ejs setting, user.routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,14 @@ const express = require("express");
 const auth_middlewares = require("../middlewares/auth-middlewares");
 const router = express.Router();
 const { getUsers, createUser, updateUser, deleteUser } = require("../controllers/user.controller");
+const {Users} = require('../models/index.js')
+const { Op } = require("sequelize");
 
 
+router.get('/points/:userId',getUserPoints = async (req,res)=>{
+
+   const users = await Users.findAll()
+   console.log(users)
+})
 
 module.exports = router;

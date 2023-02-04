2690b42722846361b59066f6093cd5f7e34eea4a
reggias <reggias010@gmail.com>
2023-02-04 19:28:34 +0900

    회원가입 3계층 분리

--- app.js
+++ app.js
@@ -1,37 +1,33 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const { createServer } = require('http');
-const path = require('path')
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const { createServer } = require("http");
+const path = require("path");
 
 // const userRouter = require("./routes/user.routes");
 
 const app = express();
-const port = 1004
+const port = 1004;
 
 // socket
 const http = createServer(app);
 
-const router = require('./routes');
+const router = require("./routes");
 
 /* ejs setting*/
-app.set('view engine','ejs');
-app.set('views', './views')
-app.use(express.static(path.join(__dirname,'views')));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static(path.join(__dirname, "views")));
 
 /* router */
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/api', router);
+app.use("/api", router);
 
-app.get('/',(req,res) => {
-    res.render('login');
-})
+app.get("/", (req, res) => {
+  res.render("login");
+});
 
 http.listen(port, () => {
-    console.log(`${port} 포트가 열렸습니다!`);
-})
-
-
-
-
+  console.log(`${port} 포트가 열렸습니다!`);
+});
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,23 +1,32 @@
 const express = require("express");
 const auth_middlewares = require("../middlewares/auth-middlewares");
 const router = express.Router();
-const { getUsers, createUser, updateUser, deleteUser, login } = require("../controllers/user.controller");
-const {Users} = require('../models/index.js')
+const {
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+  login,
+} = require("../controllers/user.controller");
+const { Users } = require("../models/index.js");
 const { Op } = require("sequelize");
 
-const UserController = require('../controllers/user.controller.js')
-const userController = new UserController()
+const UserController = require("../controllers/user.controller.js");
+const userController = new UserController();
+
+// 회원가입
+router.post("/signup", userController.registerUser);
 
 // 이름과 핸드폰 번호로 아이디 찾기
-router.get('/findBy/:name/:phone', userController.findByNameAndPhone)
+router.get("/findBy/:name/:phone", userController.findByNameAndPhone);
 
 // ID, 이름, 휴대폰 번호로 비밀번호 재설정
-router.put('/password', userController.putPasswordByIdNamePhone)
+router.put("/password", userController.putPasswordByIdNamePhone);
 
 //userId로 소지 포인트 조회
-router.get('/points/:userId', userController.getMyPoint)
+router.get("/points/:userId", userController.getMyPoint);
 
 //userId로 내 정보 변경
-router.put('/update/:userId', userController.updateUser)
+router.put("/update/:userId", userController.updateUser);
 
 module.exports = router;

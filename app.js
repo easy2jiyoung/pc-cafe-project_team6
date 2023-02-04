2ee5e028bc534acfc33cdf5e0df2cfc74a829840
kangharu <harucometrue7@gmail.com>
2023-02-04 19:46:40 +0900

    updateMyInfo ejs

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api', router);
 
 app.get('/',(req,res) => {
-    res.render('login');
+    res.render('updateMyInfo');
 })
 
 http.listen(port, () => {

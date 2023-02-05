6f45a71e6584ffd2beb151471ce7ceb3884656ce
Jin Bae <jinbae00@gmail.com>
2023-02-05 22:03:11 +0900

    pull 전 commit

---
+++ repositories/productOrder.repository.js
@@ -0,0 +1,16 @@
+class ProductOrderRepository {
+    constructor(ProductOrderModel) {
+        this.productOrderModel = ProductOrderModel
+    }
+
+    // 상품 구매 등록
+    postProductOrder = async (req, res) => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
+
+module.exports = ProductOrderRepository;

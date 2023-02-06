7e078831bac897c71ebca059b2c4688bf136efc6
reggias <reggias010@gmail.com>
2023-02-06 23:36:07 +0900

    아직 아무것도 안했음

--- views/js/admin.js
+++ views/js/admin.js
@@ -30,3 +30,36 @@ function logOut() {
     },
   });
 }
+
+// 소켓 방 입장
+const socket = io.connect("/");
+const userRole = "<%= user.role %>";
+socket.emit("newRoom", userRole);
+
+socket.on("newOrder", (message) => {
+  message = JSON.parse(message);
+  console.log(message);
+  for (item of message) {
+    let {
+      userId,
+      pcId,
+      purchasedItem,
+      qty,
+      userPoints,
+      deductedPoints,
+      remainingPoints,
+      logTime,
+    } = item;
+    logTime = new Date(logTime);
+    logTime = logTime.toLocaleString("ko-KR");
+
+    let temp_html;
+    if (purchasedItem === "이용시간") {
+      temp_html = `<p>${logTime}&emsp;PC #${pcId}의 이용시간이 ${qty}시간 결제되었습니다. (차감 포인트: ${deductedPoints}p)</p>`;
+    } else {
+      temp_html = `<p>${logTime}&emsp;PC #${pcId}이 ${purchasedItem} ${qty}개를 구매하셨습니다. (차감 포인트: ${deductedPoints}p)</p>`;
+    }
+
+    $("#logs").prepend(temp_html);
+  }
+});
